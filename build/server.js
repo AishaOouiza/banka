54f86e9864d9268d4be0781c4f7334b79b3e7516
AishaOouiza <46681001+AishaOouiza@users.noreply.github.com>
2019-04-18 09:13:55 +0100

    updated server.js

--- build/server.js
+++ build/server.js
@@ -1,29 +1,38 @@
-'use strict';
+"use strict";
 
-var _express = require('express');
+var _express = _interopRequireDefault(require("express"));
 
-var _express2 = _interopRequireDefault(_express);
+var _User = _interopRequireDefault(require("./api/models/User"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var app = (0, _express2.default)(); // server.js
-
-
-app.use(_express2.default.json());
-
+// server.js
+var app = (0, _express["default"])();
+app.use(_express["default"].json());
 app.get('/', function (req, res) {
-  return res.status(200).send({ 'message': 'Banka server now running!' });
+  return res.status(200).send({
+    message: 'Banka server now running!'
+  });
 });
-app.post('/api/v1/user', User.create);
-app.get('/api/v1/user', User.getAll);
-app.get('/api/v1/user/:id', User.getOne);
-app.put('/api/v1/user/:id', User.update);
-app.delete('/api/v1/user/:id', User.delete);
-app.post('/api/v1/account', Account.create);
-app.get('/api/v1/account', Account.getAll);
-app.get('/api/v1/account/:id', Account.getOne);
-app.put('/api/v1/account/:id', Account.update);
-app.delete('/api/v1/account/:id', Account.delete);
-
+app.get('/api/v1/user', function (req, res) {
+  return res.status(200).send({
+    message: 'User API!'
+  });
+});
+app.post('/api/v1/user/create', function (req, res) {
+  return res.status(200).send(_User["default"].create());
+});
+app.get('/api/v1/user/findAll', function (req, res) {
+  return res.status(200).send(_User["default"].findAll());
+});
+app.get('/api/v1/user/:id', function (req, res) {
+  return res.status(200).send(_User["default"].getOne(req.params.id));
+});
+app.put('/api/v1/user/:id', function (req, res) {
+  return res.status(200).send(_User["default"].update(req.params.id));
+});
+app["delete"]('/api/v1/user/:id', function (req, res) {
+  return res.status(200).send(_User["default"]["delete"](req.params.id));
+});
 app.listen(3000);
 console.log('app running on port ', 3000);
