7720b2202165c67277edcbf1e55de25983a7c359
AishaOouiza <46681001+AishaOouiza@users.noreply.github.com>
2019-04-18 09:12:19 +0100

    updated server.js

--- server.js
+++ server.js
@@ -9,11 +9,31 @@ app.use(express.json());
 app.get('/', (req, res) => {
   return res.status(200).send({ message: 'Banka server now running!'});
 });
-app.post('/api/v1/user', User.create);
-app.get('/api/v1/user', User.getAll);
-app.get('/api/v1/user/:id', User.getOne);
-app.put('/api/v1/user/:id', User.update);
-app.delete('/api/v1/user/:id', User.delete);
+
+app.get('/api/v1/user', (req, res) => {
+  return res.status(200).send({ message: 'User API!' });
+});
+
+app.post('/api/v1/user/create', (req, res) => {
+  return res.status(200).send(User.create());
+});
+
+app.get('/api/v1/user/findAll', (req, res) => {
+  return res.status(200).send(User.findAll());
+});
+
+app.get('/api/v1/user/:id', (req, res) => {
+  return res.status(200).send(User.getOne(req.params.id));
+});
+
+app.put('/api/v1/user/:id', (req, res) => {
+  return res.status(200).send(User.update(req.params.id));
+});
+
+app.delete('/api/v1/user/:id', (req, res) => {
+  return res.status(200).send(User.delete(req.params.id));
+});
+
 
 app.listen(3000);
 console.log('app running on port ', 3000);
