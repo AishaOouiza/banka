bc1736dad9b70acb661efbafe4ffd2e983ffc7c7
AishaOouiza <46681001+AishaOouiza@users.noreply.github.com>
2019-04-18 15:14:24 +0100

    updated server.js

--- build/server.js
+++ build/server.js
@@ -6,6 +6,10 @@ var _bodyParser = _interopRequireDefault(require("body-parser"));
 
 var _User = _interopRequireDefault(require("./api/controllers/User"));
 
+var _Account = _interopRequireDefault(require("./api/controllers/Account"));
+
+var _Transaction = _interopRequireDefault(require("./api/controllers/Transaction"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // server.js
@@ -41,6 +45,46 @@ app.put('/api/v1/user/:id', function (req, res) {
 });
 app["delete"]('/api/v1/user/:id', function (req, res) {
   return res.status(200).send(_User["default"]["delete"](req.params.id));
+});
+app.get('/api/v1/account', function (req, res) {
+  return res.status(200).send({
+    message: 'Account API!'
+  });
+});
+app.post('/api/v1/account/create', function (req, res) {
+  return res.status(200).send(_Account["default"].create(req.body));
+});
+app.get('/api/v1/account/findAll', function (req, res) {
+  return res.status(200).send(_Account["default"].findAll());
+});
+app.get('/api/v1/account/:id', function (req, res) {
+  return res.status(200).send(_Account["default"].getOne(req.params.id));
+});
+app.put('/api/v1/account/:id', function (req, res) {
+  return res.status(200).send(_Account["default"].update(req.params.id));
+});
+app["delete"]('/api/v1/account/:id', function (req, res) {
+  return res.status(200).send(_Account["default"]["delete"](req.params.id));
+});
+app.get('/api/v1/transaction', function (req, res) {
+  return res.status(200).send({
+    message: 'Transaction API!'
+  });
+});
+app.post('/api/v1/transaction/create', function (req, res) {
+  return res.status(200).send(_Transaction["default"].create(req.body));
+});
+app.get('/api/v1/transaction/findAll', function (req, res) {
+  return res.status(200).send(_Transaction["default"].findAll());
+});
+app.get('/api/v1/transaction/:id', function (req, res) {
+  return res.status(200).send(_Transaction["default"].getOne(req.params.id));
+});
+app.put('/api/v1/transaction/:id', function (req, res) {
+  return res.status(200).send(_Transaction["default"].update(req.params.id));
+});
+app["delete"]('/api/v1/transaction/:id', function (req, res) {
+  return res.status(200).send(_Transaction["default"]["delete"](req.params.id));
 });
 app.listen(3000);
 console.log('app running on port ', 3000);
